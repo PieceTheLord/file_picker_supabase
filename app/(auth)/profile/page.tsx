b4bff654e64715b304a3f040f11c7e38d753a159
PieceTheLord <alwx202@mail.ru>
2026-02-04 19:01:47 +0300

    Theme switcher moved to profile, add phone number in footer.

--- app/(auth)/profile/page.tsx
+++ app/(auth)/profile/page.tsx
@@ -1,12 +1,16 @@
 import { AuthButton } from "@/components/auth-button";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Suspense } from "react";
 
 export default function Page() {
-  return (
-    <>
+  return <>
       <Suspense>
         <AuthButton />
       </Suspense>
+      <div className="flex gap-x-4 items-center">
+        <p className="text-2xs font-normal">Chose your theme:</p>
+        <ThemeSwitcher />
+      </div>
     </>
-  );
+  ;
 }
